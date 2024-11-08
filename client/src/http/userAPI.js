4313eedde0c3afa23ec30594738b6d0122a2971a
aleksandr912 <45685696+aleksandr912@users.noreply.github.com>
2024-11-08 12:52:00 +0300

    edit 08112024

---
+++ client/src/http/userAPI.js
@@ -0,0 +1,27 @@
+import { $host, $autHost } from "./index";
+import { jwtDecode } from "jwt-decode";
+
+export const registration = async (email, password) => {
+  const { data } = await $host.post("api/user/registration", {
+    email,
+    password,
+    role: "ADMIN",
+  });
+  localStorage.setItem("token", data.token);
+  return jwtDecode(data.token);
+};
+
+export const login = async (email, password) => {
+  const { data } = await $host.post("api/user/login", {
+    email,
+    password,
+  });
+  localStorage.setItem("token", data.token);
+  return jwtDecode(data.token);
+};
+
+export const check = async (email, password) => {
+  const { data } = await $autHost.get("api/user/auth");
+  localStorage.setItem("token", data.token);
+  return jwtDecode(data.token);
+};
