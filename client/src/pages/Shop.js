dc6adff1819a4209d3f91fa800f5a0105ad0791a
aleksandr912 <45685696+aleksandr912@users.noreply.github.com>
2025-04-09 16:21:03 +0300

    edit 09042025 and add basket

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -6,6 +6,8 @@ import DeviceList from "../components/DeviceList";
 import { observer } from "mobx-react-lite";
 import { Context } from "..";
 import { fetchBrands, fetchDevice, fetchTypes } from "../http/deviceAPI";
+import Pages from "../components/Pages";
+import DeviseStore from "../store/DeviseStore";
 
 const Shop = observer(() => {
   const { device } = useContext(Context);
@@ -13,8 +15,15 @@ const Shop = observer(() => {
   useEffect(() => {
     fetchTypes().then((data) => device.setTypes(data));
     fetchBrands().then((data) => device.setBrands(data));
-    fetchDevice().then((data) => device.setDevice(data.rows));
-  }, []);
+    fetchDevice(null, null, 1,3).then((data => {device.setDevice(data.rows)
+  device.setTotalCount(data.count);
+  }), []);
+
+  useEffect(()=>{
+    fetchDevice(device.selectedType.id, device.selectedBrand.id, device.page,3).then((data => {device.setDevice(data.rows)
+      device.setTotalCount(data.count);
+      })
+  }, [device.page, device.selectedType, device.selectedBrand])
   return (
     <Container>
       <Row className="mt-2">
@@ -25,6 +34,7 @@ const Shop = observer(() => {
           {" "}
           <BrandBar />
           <DeviceList />
+          <Pages />
         </Col>
       </Row>
     </Container>
--- client/src/store/DeviseStore.js
+++ client/src/store/DeviseStore.js
@@ -6,6 +6,9 @@ export default class DeviseStore {
     this._devices = [];
     this._selectedType = {};
     this._selectedBrand = {};
+    this._page = 1;
+    this._totalCount = 0;
+    this._limit = 3;
     makeAutoObservable(this);
   }
 
@@ -21,11 +24,21 @@ export default class DeviseStore {
   }
 
   setSelectedType(type) {
+    this.setPage(1);
     this._selectedType = type;
   }
 
   setSelectedBrand(brand) {
+    this.setPage(1);
     this._selectedBrand = brand;
+  }
+
+  setPage(page) {
+    this._page = page;
+  }
+
+  setTotalCount(count) {
+    this._totalCount = count;
   }
 
   get types() {
@@ -42,5 +55,17 @@ export default class DeviseStore {
   }
   get selectedBrand() {
     return this._selectedBrand;
+  }
+
+  get totalCount() {
+    return this._totalCount;
+  }
+
+  get page() {
+    return this._page;
+  }
+
+  get limit() {
+    return this._limit;
   }
 }
